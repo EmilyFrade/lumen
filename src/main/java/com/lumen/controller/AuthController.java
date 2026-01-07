8f06d1bab0089fd7a2af1fa352aa91b34e3f698b
Emily Frade <emilyfrade12@gmail.com>
2026-01-06 22:03:31 -0300

    feat: cria as classes de segurança e login do sistema

---
+++ src/main/java/com/lumen/controller/AuthController.java
@@ -0,0 +1,20 @@
+package com.lumen.controller;
+
+import com.lumen.model.User;
+import com.lumen.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+}
---
+++ src/main/java/com/lumen/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.lumen.security;
+
+import com.lumen.model.User;
+import com.lumen.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = repository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
