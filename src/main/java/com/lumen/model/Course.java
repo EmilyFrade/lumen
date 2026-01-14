5bd8d042df03b18139f80e09bcb28e9c75493b9c
Emily Frade <emilyfrade12@gmail.com>
2026-01-14 20:27:18 -0300

    adiciona demais campos à entidade de curso

--- src/main/java/com/lumen/model/Course.java
+++ src/main/java/com/lumen/model/Course.java
@@ -1,7 +1,13 @@
 package com.lumen.model;
 
+import com.lumen.model.enums.Category;
+import com.lumen.model.enums.CourseStatus;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,16 +27,34 @@ public class Course {
     @Column(columnDefinition = "TEXT")
     private String description;
 
-    @ManyToOne
-    @JoinColumn(name = "category_id", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Category category;
 
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CourseStatus status;
+
     @Column(nullable = false)
     private Integer progress;
 
     private Integer rating;
+
+    private String instructor;
+
+    private String duration;
+
+    @Column(nullable = false)
+    private Boolean paid;
+
+    @Column(precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    private List<Module> modules = new ArrayList<>();
 }
